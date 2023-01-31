8b04d9b98d6dab65aa5b4b9840c3c2aa44d7684b
Jean Dubois <jd-dev@laposte.net>
2023-01-31 16:14:21 +0100

    add ep5

---
+++ ep5_structures_conditionnelles/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int condition;
+
+    for (int b = 0; b != 20; b++) {
+        condition = a == b;
+        printf("a = %d\n", a);
+        printf("b = %d\n", b);
+
+        // multiline if statements
+        if (condition) {
+            printf("a est égal à b.\n");
+        } else if (a == b+1 || a+1 == b) {
+            printf("a et b sont différents, mais sont éloignés de 1\n");
+        } else {
+            printf("a est différent de b.\n");
+        }
+
+        // single line if statements
+        if (b == 15)
+            printf("b égale 15 !!\n");
+        else if (b == 18)
+            printf("b égale 18 !!\n");
+        
+        if (b == 5) printf("b égale 5 !!\n");
+        else if (b == 2) printf("b égale 2 !!\n");
+
+        printf("\n");
+    }
+
+    // this value is pretty interesting : 4294967338 ^^'
+    int c;
+    printf("Entrez la valeur de c: ");
+    scanf("%d", &c);
+    printf("c = %d\n", c);
+    switch (c) {
+        case 42:
+            printf("La réponse universelle à la vie, l'univers et le reste\n");
+            break;
+
+        case 52:
+            printf("C'est 52\n");
+            break;
+        
+        default:
+            printf("Meh, c'est pas une valeur de ouf...\n");
+    }
+
+    return 0;
+}
