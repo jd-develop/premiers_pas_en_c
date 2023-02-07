cf72e9cf8ba8b122a3acf30813b3af9f278b4b1d
Jean Dubois <jd-dev@laposte.net>
2023-02-07 08:55:20 +0100

    add ep11

---
+++ ep11_constantes/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define TAILLE_TAB 10  // macro préprocesseur
+
+int main() {
+    const int A = 10;  // constante c
+    int tab[TAILLE_TAB];
+
+    printf("Taille tableau : %d\n", TAILLE_TAB);
+    printf("A = %d\n", A);
+
+    printf("Je suis à la ligne %d\n", __LINE__);
+
+    printf("Je suis à la ligne %d dans le fichier %s\n", __LINE__, __FILE__);
+
+    return 0;
+}
