5c19ac6769c9612a0eaa7306a413919d74556516
Jean Dubois <jd-dev@laposte.net>
2024-07-18 22:46:25 +0100

    Create more_hello_world.ml

---
+++ ocaml/more_hello_world.ml
@@ -0,0 +1,12 @@
+print_endline "Hello world";;
+
+print_int (12+5); print_newline ();;
+print_int (12*5); print_newline ();;
+
+print_float 15.2; print_newline ();;
+print_float (15.2 +. 3.2); print_newline ();;
+print_float (15.2 *. 12.1); print_newline ();;
+(* comment *)
+
+(* modulo *)
+print_int (10 mod 3); print_newline ();;
