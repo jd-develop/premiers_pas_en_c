8137d939de9f973207776c740c81ea5aff58f6cd
Jean Dubois <jd-dev@laposte.net>
2024-07-18 22:58:01 +0100

    Update more_hello_world.ml

--- ocaml/more_hello_world.ml
+++ ocaml/more_hello_world.ml
@@ -10,3 +10,8 @@ print_float (15.2 *. 12.1); print_newline ();;
 
 (* modulo *)
 print_int (10 mod 3); print_newline ();;
+
+(* boolean: true/false *)
+
+(* concatenate *)
+print_endline ("hello " ^ "world");;
